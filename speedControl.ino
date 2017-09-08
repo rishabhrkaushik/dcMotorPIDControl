61813298350b6d413ad512c8fa78d8b1988e2682
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2017-09-08 23:03:19 +0530

    count number of rotations

--- speedControl.ino
+++ speedControl.ino
@@ -1,15 +1,24 @@
 // interrupt using hall effect or IR sensor on pin number 2 of Arduino
 #define interruptPin 2
 
+unsigned long rotationsCount = 0;
+
+void countInc(){
+  rotationsCount++;
+}
+
 void setup(){
   pinMode(interruptPin, INPUT);
+  attachInterrupt(0, countInc, FALLING);
   Serial.begin(9600);
   Serial.setTimeout(10);
-  delay(1000);	
+  delay(1000);  
 }
 
 void loop(){
-  Serial.print("read: ");
-  Serial.println(digitalRead(interruptPin));
+  Serial.print("Rotations: ");
+  Serial.print(rotationsCount);
+  Serial.print("    Time: ");
+  Serial.println(millis());
   delay(100);
 }
